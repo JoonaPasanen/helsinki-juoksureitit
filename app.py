9686b6f611b3f8d3bbca220f4db620724cdc131f
Joona <joona.t.pasanen@gmail.com>
2026-03-11 23:07:21 +0200

    Added database and user creating form

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
+import sqlite3
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db = sqlite3.connect("database.db")
+        db.execute(sql, [username, password_hash])
+        db.close()
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
